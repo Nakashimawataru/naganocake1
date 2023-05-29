863e5bea8400f179d622e339d1efe8c3f2d9352a
EC2 Default User <130224869+Nakashimawataru@users.noreply.github.com>
2023-05-29 10:51:17 +0000

    Initial Commit

---
+++ app/controllers/public/customers_controller.rb
@@ -0,0 +1,13 @@
+class Public::CustomersController < ApplicationController
+  def show
+  end
+  def edit
+  end
+  def update
+  end
+  def cancellation
+  end
+  def withdrawal
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,32 @@
 Rails.application.routes.draw do
-  devise_for :admins
-  devise_for :customers
+  root to: "public/homes#top"
+  get "home/about" => "public/homes#about", as: "about"
+ # 顧客用
+# URL /customers/sign_in ...
+devise_for :customers,skip: [:passwords], controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions',
+}
+
+# 管理者用
+# URL /admin/sign_in ...
+devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+  sessions: "admin/sessions"
+}
+  namespace :admin do
+   resources :items, only: [:index, :show, :create, :show, :edit, :update]
+   resources :homes, only:[:top]
+   resources :customers, only:[:index, :show, :edit, :update]
+   resources :orders, only:[:show]
+  end
+
+  scope module: :public do
+    resources :items, only: [:index, :show]
+    resources :customers, only: [:show, :edit, :update, :cancellation, :withdrawal]
+    resources :cart_items, only: [:index, :update, :destroy, :destroy_all, :create]
+    resources :orders, only: [:new, :confirm, :completion, :create, :index, :show]
+  end
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
